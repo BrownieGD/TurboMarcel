3b695c7bc1c30a094d6f9bce13755bd9470cc38b
Lasse Wind <lasse.wind@gmx.net>
2024-06-02 20:52:39 +0200

    Added some Stuff to work further on

--- tuma.js
+++ tuma.js
@@ -1,17 +1,43 @@
 const fs = require('fs');
 const ermahnung = "Lass dir das von Marcel nochmal erklären!";
 
-const fnRegex = //g;
+const mainFnRegex = /test/g;
+const fnRegex = /test/g;
+const variableRegex = /test/g;
 
+
+/*
+    Example Func:
+        .Funktion (param1) HelloWorld{
+            println("Hello World");
+        }.
+*/
 
 function replaceLineBreaks(code) {
     return code.replace(/\r\n/g, '');
 }
 
+function writeCodeToFile(code) {
+    if (code) {
+        fs.writeFile('out.js', code, { flag: 'wx' }, err => {
+            if (err) {
+                console.error(err);
+            }
+        });
+    }
+}
+
 function compile(code) {
-    //Use Regular Expression to find all parts of the code
+    let w = "";
+    //Search for main
+    const main = replaceLineBreaks(code).match(mainFnRegex);
+    w += main;
+    // Search for Funcs
     const functions = replaceLineBreaks(code).match(fnRegex);
-    console.log(functions);
+    w += functions;
+
+    //Finally write to file
+    writeCodeToFile(w);
 }
 
 
