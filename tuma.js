bc0251a78270ab459796b9a139080d348ad29e8d
Lasse Wind <lasse.wind@gmx.net>
2024-06-03 15:32:22 +0200

    Variables are working now I guess

--- tuma.js
+++ tuma.js
@@ -1,17 +1,36 @@
 const fs = require('fs');
 const ermahnung = "Lass dir das von Marcel nochmal erklären!";
 
-const mainFnRegex = /test/g;
-const fnRegex = /test/g;
-const variableRegex = /test/g;
+const mainFnRegex = /\.Funktion.*\./g;
+const fnRegex = /\.Main.*\./g;
+const variableRegex = /\.Variable\s.*?\./g;
 
 
 /*
     Example Func:
-        .Funktion (param1) HelloWorld{
+        .Funktion (param1) HelloWorld {
             println("Hello World");
         }.
+
+    Example Variable:
+        .Variable test = "Hello World".
 */
+
+function tumaFunToJsFun(code) {
+    let jsFun = "";
+
+    return jsFun;
+}
+
+
+function tumaVarToJsVar(code) {
+    let jsVar = "let";
+    let i = code.match(/.*=.*/)[0];
+    i = i.replace(/\./g, "");
+    i = i.replace(/Variable/g, "");
+    jsVar += i + ";";
+    return jsVar;
+}
 
 function replaceLineBreaks(code) {
     return code.replace(/\r\n/g, '');
@@ -31,11 +50,13 @@ function compile(code) {
     let w = "";
     //Search for main
     const main = replaceLineBreaks(code).match(mainFnRegex);
-    w += main;
     // Search for Funcs
     const functions = replaceLineBreaks(code).match(fnRegex);
-    w += functions;
-
+    // Search for Variables
+    const variables = replaceLineBreaks(code).match(variableRegex);
+    variables.forEach(element => {
+        w += tumaVarToJsVar(element);
+    });
     //Finally write to file
     writeCodeToFile(w);
 }
